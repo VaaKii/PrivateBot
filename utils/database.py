60b70e699001911986c3ecb7a5e1654925578f7f
Renegatto <46404781+Renegatto@users.noreply.github.com>
2021-02-10 22:33:11 +0300

    Update database.py
    FP in every house.

--- utils/database.py
+++ utils/database.py
@@ -5,61 +5,58 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def select(table,lines,criterion:str = None) -> str:
-    connect = connentor.connect(user=os.getenv('user'),database=os.getenv('database'),passwd=os.getenv('passwd'),host=os.getenv('host'))
-    cursor = connect.cursor()
-    what_in_database = ""
-    for row in lines:
-        if what_in_database == "":
-            what_in_database += f"`{str(row)}`"
-        else:
-            what_in_database += f", `{str(row)}`"
-    if criterion != None:
-        send = f"SELECT {what_in_database} FROM {table} WHERE {criterion}"
-    else:
-        send = f"SELECT {what_in_database} FROM {table}"
+def connection():
+     conn = connentor.connect(
+        user=os.getenv('user'),
+        database=os.getenv('database'),
+        passwd=os.getenv('passwd'),
+        host=os.getenv('host'))
+     return (conn,conn.cursor())
+
+def commitWith(conn,cursor,f=lambda:true):
     cursor.execute(send)
-    result = cursor.fetchall()
-    connect.commit()
+    result = f()
+    connection.commit()
     return result
+
+def listKeysValues(keys:str,values:str) -> str :
+    show_pair = lambda kv: f"{kv[0]} = '{kv[1]}'"
+    return (',\n').join(map(show_pair, zip(keys,values)))
+
+def select(table,lines,criterion:str = None) -> str:
+    connect, cursor = connection()
+    show_elem = lambda x: f"`{x}`"
+    
+    db_content = (,).join(map(show_elem,lines))
+    
+    if criterion != None:
+        send = f"SELECT {db_content} FROM {table} WHERE {criterion}"
+    else:
+        send = f"SELECT {db_content} FROM {table}"
+    return commitWith(cursor,connect,lambda:cursor.fetchall())
+
 def insert(table,lines,values):
-    connect = connentor.connect(user=os.getenv('user'),database=os.getenv('database'),passwd=os.getenv('passwd'),host=os.getenv('host'))
-    cursor = connect.cursor()
-    what_in_database = ""
-    for row in lines:
-        if what_in_database == "":
-            what_in_database += f"`{str(row)}`"
-        else:
-            what_in_database += f", `{str(row)}`"
-    values_in_database = ""
-    for row in values:
-        if values_in_database == "":
-            values_in_database += f"'{str(row)}'"
-        else:
-            values_in_database += f", '{str(row)}'"
-    send = f"INSERT INTO {table} ({what_in_database}) VALUES ({values_in_database})"
-    cursor.execute(send)
-    connect.commit()
+    connect, cursor = connection()
+    
+    show_elem = lambda x: f"`{x}`"
+    
+    db_content = (,).join(map(show_elem,lines))
+    db_values = (,).join(map(show_elem,values))
+
+    send = f"INSERT INTO {table} ({db_content}) VALUES ({db_values})"
+    
+    commitWith(cursor,connect)
 
 def update(table,column:str,value:str,criterion):
-    connect = connentor.connect(user=os.getenv('user'),database=os.getenv('database'),passwd=os.getenv('passwd'),host=os.getenv('host'))
-    cursor = connect.cursor()
-    column_in_database = ""
-    for i in range(len(column)):
-        if column_in_database == "":
-            column_in_database += f"{column[i]} = '{value[i]}'"
-        else:
-            column_in_database += f",\n{column[i]} = '{value[i]}'"
+    connect, cursor = connection()
+    
     send = f"""UPDATE {table}
-            SET {column_in_database}
+            SET {listKeysValues(column,value)}
             WHERE {criterion}
             """
-    cursor.execute(send)
-    connect.commit()
+    commitWith(cursor,connect)
 
 def delete(table,where):
-    connect = connentor.connect(user=os.getenv('user'),database=os.getenv('database'),passwd=os.getenv('passwd'),host=os.getenv('host'))
-    cursor = connect.cursor()
+    connect, cursor = connection()
     send = f"DELETE FROM {table} WHERE {where}"
-    cursor.execute(send)
-    connect.commit()
+    commitWith(cursor,connect)
